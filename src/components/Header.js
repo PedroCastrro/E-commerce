3cfbf109298bb62f0ac1d8bdfddea67e2ea46a6d
Pedro Castro <pgkastro@gmail.com>
2022-10-31 19:00:06 -0300

    Styles components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar, Ul, Li} from '../styles/styles';
+
+import Logo from '../imagem/logo.svg'
+import Sale from '../imagem/icon-cart.svg'
+import Avatar from '../imagem/image-avatar.png'
 
 export default function Header() {
-    return (
-        <div>
-            <header>
-                <nav>
-                    <ul>
-                        <Link to="/">Collections</Link>
-                        <Link to="/Men">Men</Link>
-                        <Link to="/Women">Women</Link>
-                        <Link to="/About">About</Link>
-                        <Link to="/Conect">Conect</Link>
-                    </ul>
-                </nav>
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar>
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
+        <Ul>
+          <Li>
+            <Link to="/">Collections</Link>
+          </Li>
+          <Li>
+            <Link to="/Men">Men</Link>
+          </Li>
+          <Li>
+            <Link to="/Women">Women</Link>
+          </Li>
+          <Li>
+            <Link to="/About">About</Link>
+          </Li>
+          <Li>
+            <Link to="/Conect">Conect</Link>
+          </Li>
+        </Ul>
+        <img src={Sale} alt="" />
+        <img src={Avatar} alt="" />
+      </Navbar>
+    </div>
+  )
 }
---
+++ src/styles/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Navbar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+`;
+
+export const Ul = styled.ul`
+    display: flex;
+    list-style: none;
+    align-items: top;
+`;
+
+export const Li = styled.li`
+    margin-right: 1em;
+`;
+
