50e50137a55ac33a7a8d047be903a997f0f57982
Pedro Castro <pgkastro@gmail.com>
2022-11-01 08:35:04 -0300

    Add image

--- src/pages/Collections.js
+++ src/pages/Collections.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react'
 
-import Header from '../components/Header';
+import Product1 from '../imagem/image-product-1.jpg'
+import Product2 from '../imagem/image-product-2.jpg'
+import Product3 from '../imagem/image-product-3.jpg'
+import Product4 from '../imagem/image-product-4.jpg'
+
+import {Body, Section, Img1, Img, Aside} from '../styles/styles'
+
+import Header from '../components/Header'
 
 function Collections() {
+
   return (
-    <div className="App">
-        <Header />
-     <h2>sdasd</h2>
+    <div>
+      <Header />
+      <Body>
+        <Section>
+          <Img1 src={Product1} alt="" />
+          <Img src={Product1} alt="" />
+          <Img src={Product2} alt="" />
+          <Img src={Product3} alt="" />
+          <Img src={Product4} alt="" />
+        </Section>
+        <Aside>
+
+        </Aside>
+      </Body>
     </div>
-  );
+  )
 }
 
-export default Collections;
+export default Collections
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -16,3 +16,27 @@ export const Li = styled.li`
     margin-right: 1em;
 `;
 
+export const Body = styled.body`
+    display: flex;
+    justify-content: center;
+`;
+
+export const Section = styled.section`
+    
+`;
+
+export const Img1 = styled.img`
+    height: 300px;
+    display: block;
+    border-radius: 6%;
+`;
+
+export const Img = styled.img`
+    height: 100px;
+    display: inline-block;
+    border-radius: 6%; 
+`;
+
+export const Aside = styled.section`
+
+`;
