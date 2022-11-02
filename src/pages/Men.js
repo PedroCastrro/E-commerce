d98e53262879afea15c9781bb56f0c6871012153
Pedro Castro <pgkastro@gmail.com>
2022-11-02 08:16:55 -0300

    Woman page

--- src/pages/Men.js
+++ src/pages/Men.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import Men0 from '../imagem/men.jpg';
+import Men1 from '../imagem/men1.jpg';
+import Men2 from '../imagem/men2.webp';
+import Men3 from '../imagem/men3.jpeg';
+import Men4 from '../imagem/men4.jpg';
+
 import Header from '../components/Header';
 
 function Men() {
   return (
-    <div className="App">
+    <div>
         <Header />
-     <h2>sdasd</h2>
+     <body>
+      <main>
+        <section>
+          <img src={ Men0 } alt="" />
+          <img src={ Men1} alt="" />
+          <img src={ Men2 } alt="" />
+          <img src={ Men3} alt="" />
+          <img src={ Men4} alt="" />
+        </section>
+      </main>
+     </body>
     </div>
   );
 }
--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import Women0 from '../imagem/Women.jpg';
+import Women1 from '../imagem/Women1.webp';
+import Women2 from '../imagem/Women2.webp';
+import Women3 from '../imagem/Women3.jpg';
+import Women4 from '../imagem/Women4.webp';
+
 import Header from '../components/Header';
 
 function Women() {
   return (
-    <div className="App">
+    <div>
         <Header />
-     <h2>sdasd</h2>
+      <body>
+        <main>
+          <img src={ Women0 } alt="" />
+          <img src={ Women1 } alt="" />
+          <img src={ Women2 } alt="" />
+          <img src={ Women3 } alt="" />
+          <img src={ Women4 } alt="" />
+        </main>
+      </body>
     </div>
   );
 }
